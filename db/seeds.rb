909f0aa6712e159ce6b19e67f2d44654c5d3e266
clement bergantz <bergantz.clement@gmail.com>
2017-08-09 16:52:43 +0200

    all features added : list all restaurants, view details, add a new restaurant, add a new review and display the review with the details of the restaurant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Les 2 stations', address: 'rue de billancourt', category: 'french')
+Restaurant.create(name: 'Napoli', address: 'rue du bac', category: 'italian')
+Restaurant.create(name: 'Le bouchon français', address: 'rue de Paris', category: 'french')
+Restaurant.create(name: 'Sushi', address: "rue de l'ermitage", category: 'japanese')
+Restaurant.create(name: "Aux delices d'Asie", address: 'rue du soleil levant', category: 'chinese')
